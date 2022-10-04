1f1d902895a79c05db9ac20027c560885b58b1d6
NorSP <norbissilvera@gmail.com>
2022-10-04 20:47:30 +0000

    comentario

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+// import ReactDOM from 'react-dom';
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+    return (
+<div className="container text-center">
+  <div className="row text-white bg-dark">
+    <div className="col m-5 pb-5 border border-2 rounded-5">
+	<h1><i className="far fa-clock"></i></h1>
+    </div>
+    <div className="col m-5 pt-5 pb-5 border border-2 rounded-4">
+    <h1>{props.hora}</h1>
+    </div>
+    <div className="col m-5 pt-5 pb-5 border border-2 rounded-4">
+    <h1>{props.minuto2}</h1>
+    </div>
+    <div className="col m-5 pt-5 pb-5 border border-2 rounded-4">
+    <h1>{props.minuto}</h1>
+    </div>
+    <div className="col m-5 pt-5 pb-5 border border-2 rounded-4">
+    <h1>{props.segundos2}</h1>
+    </div>
+    <div className="col m-5 pt-5 pb-5 border border-2 rounded-5">
+    <h1>{props.segundos}</h1>
+    </div>
+  </div>
+</div>
+    );
 };
-
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
